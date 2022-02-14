96e63eb63c9f8bfe19e007257f640748ead93b1d
Micha Davis <m.leigh.davis@gmail.com>
2022-02-14 15:22:30 -0800

    add footer component to app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from './Footer';
 import Header from './Header';
 import Main from './Main';
 
@@ -8,7 +9,7 @@ class App extends React.Component {
       <>
         <Header />
         <Main />
-        
+        <Footer />
       </>
     );
   }

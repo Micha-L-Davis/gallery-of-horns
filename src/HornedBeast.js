9f972a4b3f7a9dcd98e29068624a196a3286a838
Micha Davis <m.leigh.davis@gmail.com>
2022-02-14 15:23:05 -0800

    added classes to image and parent div

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -3,11 +3,11 @@ import React from 'react';
 class HornedBeast extends React.Component {
     render(){
         return (
-            <>
+            <div class="beast">
                 <h2>{this.props.title}</h2>
-                <img src={this.props.imageURL} alt={this.props.description} />
+                <img src={this.props.imageURL} alt={this.props.description} class="beast-image" />
                 <p>{this.props.description}</p>
-            </>
+            </div>
         );
     }
 }

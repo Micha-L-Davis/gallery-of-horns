cdff4377f9f7d936984c7ea83a72f3476fd75199
Micha Davis <m.leigh.davis@gmail.com>
2022-02-14 15:23:48 -0800

    add test images

--- src/Main.js
+++ src/Main.js
@@ -7,12 +7,12 @@ class Main extends React.Component {
             <main>
                 <HornedBeast 
                     title = 'Beast 1' 
-                    imageURL = 'img/beast1.png'
+                    imageURL = 'https://images.unsplash.com/photo-1601393977410-65950a43f1b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1959&q=80'
                     description = 'Beast 1'
                 />
                 <HornedBeast 
                     title = 'Beast 2' 
-                    imageURL = 'img/beast2.png'
+                    imageURL = 'https://images.unsplash.com/photo-1549471013-3364d7220b75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
                     description = 'Beast 2'
                 />
             </main>

9e795eea596c4820076f1d9a791e6686b3252e7c
Micha Davis <m.leigh.davis@gmail.com>
2022-02-16 14:26:28 -0800

    added modal related state, methods and conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,22 +2,38 @@ import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
 import Main from './Main';
+import SelectedBeast from './SelectedBeast'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import data from './data.json';
+import Modal from 'react-bootstrap/Modal';
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       selectedBeast: 'none',
+      showModal: false
     }
   }
 
-  handleSelectBeast = (beast) => this.setState({ selectedBeast: beast });
+  handleSelectBeast = (beastIndex) => {
+    this.setState({ selectedBeast: data[beastIndex] });
+    this.setState({ showModal: true });
+  };
+
+  handleCloseModal = () => this.setState({ showModal: false });
 
   render(){
     return (
       <>
+        <Modal show={this.state.showModal} onHide={this.handleCloseModal}>
+          <SelectedBeast 
+            title={this.state.selectedBeast.title}
+            imageURL={this.state.selectedBeast.image_url}
+            description={this.state.selectedBeast.description}
+          />
+        </Modal>
+
         <Header />
         <Main data={data} handleSelectBeast={this.handleSelectBeast}/>
         <Footer />

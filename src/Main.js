4f951df8beb35719a164848727ff38ec92f04a40
Micha Davis <m.leigh.davis@gmail.com>
2022-02-16 13:25:49 -0800

    import data.json file into App component and send to Main component

--- src/Main.js
+++ src/Main.js
@@ -2,16 +2,13 @@ import React from 'react';
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row';
 import HornedBeast from './HornedBeast';
-import data from './data.json';
-
-
 
 class Main extends React.Component {
   render() {
     return (
       <main>
           <Row  xs={1} md={2} lg={3}>
-            {data.map((beast, index) => (
+            {this.props.data.map((beast, index) => (
                 <Col>
                   <HornedBeast
                     title={beast.title}

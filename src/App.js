39bd1b18983ddf942e5d916cb67a26c7b33e32cd
Micha Davis <m.leigh.davis@gmail.com>
2022-02-16 13:26:07 -0800

    Map over the JSON data in Main component to render each beast

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Footer from './Footer';
 import Header from './Header';
 import Main from './Main';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import data from './data.json';
 
 class App extends React.Component {
   render(){
     return (
       <>
         <Header />
-        <Main />
+        <Main data={data}/>
         <Footer />
       </>
     );

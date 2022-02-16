3144632f5821939d8681735634631d040b6d233c
Micha Davis <m.leigh.davis@gmail.com>
2022-02-16 14:28:44 -0800

    added passing function prop on to child

--- src/Main.js
+++ src/Main.js
@@ -10,13 +10,15 @@ class Main extends React.Component {
           <Row  xs={1} md={2} lg={3}>
             {this.props.data.map((beast, index) => (
                 <Col>
-                  <HornedBeast
-                    title={beast.title}
+                  <HornedBeast                    
                     key={index}
+                    title={beast.title}
+                    index={index}
                     imageURL={beast.image_url}
                     description={beast.description}
                     keyword={beast.keyword}
                     horns={beast.horns}
+                    handleSelectBeast={this.props.handleSelectBeast}
                   />
                 </Col>
               )

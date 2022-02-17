31bb92299800fa0e89a525ed4fdf164941d96f0f
Micha Davis <m.leigh.davis@gmail.com>
2022-02-17 14:08:02 -0800

    add form to select data filters, method to send selection to app

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form'
 
 class Header extends React.Component {
+  handleFilter = event => this.props.filterGallery(event.target.value === 'no-filter'? 0 : parseInt(event.target.value));
+
   render() {
     return (
       <header>
         <h1>Gallery of Horns</h1>
+        <Form>
+          <Form.Group onChange={this.handleFilter}>
+            <Form.Label>Filter Gallery Display</Form.Label>
+            <Form.Select>
+              <option value="no-filter">no filter...</option>
+              <option value="1">one horn</option>
+              <option value="2">two horns</option>
+              <option value="3">three horns</option>
+              <option value="100">one-hundred horns</option>
+            </Form.Select>
+          </Form.Group>
+        </Form>
       </header>
     );
   }
